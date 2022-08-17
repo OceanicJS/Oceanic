4fbd750141503ca6dda12ffa89536eec4ad62d80
Donovan Daniels <hewwo@yiff.rocks>
2022-08-17 05:45:47 -0500

    make `Properties` dynamic

--- lib/util/Properties.ts
+++ lib/util/Properties.ts
@@ -1,18 +1,27 @@
-export default class Properties {
+export default class Properties<C = unknown> {
 	private obj: unknown;
 	constructor(obj: unknown) {
 		this.obj = obj;
 	}
 
-	static define(obj: unknown, property: string, value: unknown, writable = false, configurable = false, enumerable = false) {
-		return new Properties(obj).define(property, value, writable, configurable, enumerable);
+	static define<T, K extends keyof T>(obj: T, property: K, value: T[K], writable = false, configurable = false, enumerable = false) {
+		return new Properties<T>(obj).define(property, value, writable, configurable, enumerable);
 	}
 
-	static new(obj: unknown) {
-		return new Properties(obj);
+	static looseDefine<T>(obj: T, property: string, value: unknown, writable = false, configurable = false, enumerable = false) {
+		return new Properties<T>(obj).looseDefine(property, value, writable, configurable, enumerable);
 	}
 
-	define(property: string, value: unknown, writable = false, configurable = false, enumerable = false) {
+	static new<T>(obj: T) {
+		return new Properties<T>(obj);
+	}
+
+	define<K extends keyof C>(property: K, value: C[K], writable = false, configurable = false, enumerable = false) {
+		Object.defineProperty(this.obj, property, { value, writable, configurable, enumerable });
+		return this;
+	}
+
+	looseDefine(property: string, value: unknown, writable = false, configurable = false, enumerable = false) {
 		Object.defineProperty(this.obj, property, { value, writable, configurable, enumerable });
 		return this;
 	}
