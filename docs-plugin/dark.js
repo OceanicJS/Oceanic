a6b22ebd409881342899ae8f892214a0968877ee
Donovan Daniels <hewwo@yiff.rocks>
2022-09-02 01:37:27 -0500

    remove typedoc stuff from lib

--- docs-plugin/dark.js
+++
@@ -1,9 +0,0 @@
-const { JSX } = require("typedoc");
-
-// https://github.com/TypeStrong/typedoc/issues/1840#issuecomment-1012736455
-exports.load = function load(app) {
-    app.renderer.hooks.on("head.begin", () => {
-        return JSX.createElement("script", null,
-          JSX.createElement(JSX.Raw, { html: "localStorage.setItem('tsd-theme', localStorage.getItem('tsd-theme') || 'dark')" }));
-    });
-}
