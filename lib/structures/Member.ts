bcdbabd1626e03b5ad71df2d775b7228d3dc7a85
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2022-09-09 22:41:19 -0300

    Check if guild is present before updating clientMember

--- lib/structures/Member.ts
+++ lib/structures/Member.ts
@@ -106,7 +106,7 @@ export default class Member extends Base {
             this.user = this.client.users.update(data.user);
         }
 
-        if (this.id === this.client.user.id) {
+        if (this.guild && this.id === this.client.user.id) {
             if (this.guild["_clientMember"]) {
                 this.guild["_clientMember"].update(data);
             } else {
