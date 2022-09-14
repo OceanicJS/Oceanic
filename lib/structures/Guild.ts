4e53781805e2798a71f6a71a3230f869f6c634a3
Donovan Daniels <hewwo@yiff.rocks>
2022-09-14 11:17:14 -0500

    named tuple

--- lib/structures/Guild.ts
+++ lib/structures/Guild.ts
@@ -140,7 +140,7 @@ export default class Guild extends Base {
     /** The icon hash of this guild. */
     icon: string | null;
     /** The integrations in this guild. */
-    integrations: TypedCollection<string, RawIntegration, Integration, [string?]>;
+    integrations: TypedCollection<string, RawIntegration, Integration, [guildID?: string]>;
     /** The date at which this guild was joined. */
     joinedAt: Date | null;
     /** If this guild is considered large. */
