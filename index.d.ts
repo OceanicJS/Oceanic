50fea32a4c34654b9756bf8e267044d2e4cc1cf4
Donovan Daniels <hewwo@yiff.rocks>
2022-09-20 15:02:41 -0500

    try that on for size

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import * as _exports from "./dist/lib/index";
+export * from "./dist/lib/index";
+export default _exports;
--- lib/index.ts
+++ lib/index.ts
@@ -1,134 +1,67 @@
-// Channel and Interaction MUST be at the top due to circular imports
-import Channel from "./structures/Channel";
-import Interaction from "./structures/Interaction";
-import AnnouncementChannel from "./structures/AnnouncementChannel";
-import AnnouncementThreadChannel from "./structures/AnnouncementThreadChannel";
-import Application from "./structures/Application";
-import ApplicationCommand from "./structures/ApplicationCommand";
-import Attachment from "./structures/Attachment";
-import AuditLogEntry from "./structures/AuditLogEntry";
-import AutocompleteInteraction from "./structures/AutocompleteInteraction";
-import AutoModerationRule from "./structures/AutoModerationRule";
-import Base from "./structures/Base";
-import Bucket from "./rest/Bucket";
-import CategoryChannel from "./structures/CategoryChannel";
-import Client from "./Client";
-import ClientApplication from "./structures/ClientApplication";
-import * as Constants from "./Constants";
-import CommandInteraction from "./structures/CommandInteraction";
-import Collection from "./util/Collection";
-import ComponentInteraction from "./structures/ComponentInteraction";
-import DiscordHTTPError from "./rest/DiscordHTTPError";
-import DiscordRESTError from "./rest/DiscordRESTError";
-import ExtendedUser from "./structures/ExtendedUser";
-import ForumChannel from "./structures/ForumChannel";
-import GatewayError from "./gateway/GatewayError";
-import GroupChannel from "./structures/GroupChannel";
-import Guild from "./structures/Guild";
-import GuildChannel from "./structures/GuildChannel";
-import GuildPreview from "./structures/GuildPreview";
-import GuildScheduledEvent from "./structures/GuildScheduledEvent";
-import GuildTemplate from "./structures/GuildTemplate";
-import Integration from "./structures/Integration";
-import InteractionResolvedChannel from "./structures/InteractionResolvedChannel";
-import InteractionOptionsWrapper from "./util/InteractionOptionsWrapper";
-import Invite from "./structures/Invite";
-import Member from "./structures/Member";
-import Message from "./structures/Message";
-import ModalSubmitInteraction from "./structures/ModalSubmitInteraction";
-import PartialApplication from "./structures/PartialApplication";
-import Permission from "./structures/Permission";
-import PermissionOverwrite from "./structures/PermissionOverwrite";
-import PingInteraction from "./structures/PingInteraction";
-import PrivateChannel from "./structures/PrivateChannel";
-import PrivateThreadChannel from "./structures/PrivateThreadChannel";
-import PublicThreadChannel from "./structures/PublicThreadChannel";
-import RESTManager from "./rest/RESTManager";
-import Role from "./structures/Role";
-import * as Routes from "./util/Routes";
-import SequentialBucket from "./rest/SequentialBucket";
-import Shard from "./gateway/Shard";
-import ShardManager from "./gateway/ShardManager";
-import StageChannel from "./structures/StageChannel";
-import StageInstance from "./structures/StageInstance";
-import Team from "./structures/Team";
-import TextableChannel from "./structures/TextableChannel";
-import TextChannel from "./structures/TextChannel";
-import ThreadChannel from "./structures/ThreadChannel";
-import TypedCollection from "./util/TypedCollection";
-import TypedEmitter from "./util/TypedEmitter";
-import UnavailableGuild from "./structures/UnavailableGuild";
-import User from "./structures/User";
-import Util from "./util/Util";
-import VoiceChannel from "./structures/VoiceChannel";
-import VoiceState from "./structures/VoiceState";
-import Webhook from "./structures/Webhook";
-
 export * from "./types/index";
+// Channel and Interaction MUST be at the top due to circular imports
+export { default as Channel } from "./structures/Channel";
+export { default as Interaction } from "./structures/Interaction";
+export { default as AnnouncementChannel } from "./structures/AnnouncementChannel";
+export { default as AnnouncementThreadChannel } from "./structures/AnnouncementThreadChannel";
+export { default as Application } from "./structures/Application";
+export { default as ApplicationCommand } from "./structures/ApplicationCommand";
+export { default as Attachment } from "./structures/Attachment";
+export { default as AuditLogEntry } from "./structures/AuditLogEntry";
+export { default as AutocompleteInteraction } from "./structures/AutocompleteInteraction";
+export { default as AutoModerationRule } from "./structures/AutoModerationRule";
+export { default as Base } from "./structures/Base";
+export { default as Bucket } from "./rest/Bucket";
+export { default as CategoryChannel } from "./structures/CategoryChannel";
+export { default as Client } from "./Client";
+export { default as ClientApplication } from "./structures/ClientApplication";
 export * from "./Constants";
-export {
-    Channel,
-    Interaction,
-    AnnouncementChannel,
-    AnnouncementThreadChannel,
-    Application,
-    ApplicationCommand,
-    Attachment,
-    AuditLogEntry,
-    AutocompleteInteraction,
-    AutoModerationRule,
-    Base,
-    Bucket,
-    CategoryChannel,
-    Client,
-    ClientApplication,
-    Constants,
-    CommandInteraction,
-    Collection,
-    ComponentInteraction,
-    DiscordHTTPError,
-    DiscordRESTError,
-    ExtendedUser,
-    ForumChannel,
-    GatewayError,
-    GroupChannel,
-    Guild,
-    GuildChannel,
-    GuildPreview,
-    GuildScheduledEvent,
-    GuildTemplate,
-    Integration,
-    InteractionResolvedChannel,
-    InteractionOptionsWrapper,
-    Invite,
-    Member,
-    Message,
-    ModalSubmitInteraction,
-    PartialApplication,
-    Permission,
-    PermissionOverwrite,
-    PingInteraction,
-    PrivateChannel,
-    PrivateThreadChannel,
-    PublicThreadChannel,
-    RESTManager,
-    Role,
-    Routes,
-    SequentialBucket,
-    Shard,
-    ShardManager,
-    StageChannel,
-    StageInstance,
-    Team,
-    TextableChannel,
-    TextChannel,
-    ThreadChannel,
-    TypedCollection,
-    TypedEmitter,
-    UnavailableGuild,
-    User,
-    Util,
-    VoiceChannel,
-    VoiceState,
-    Webhook
-};
+export * as Constants from "./Constants";
+export { default as CommandInteraction } from "./structures/CommandInteraction";
+export { default as Collection } from "./util/Collection";
+export { default as ComponentInteraction } from "./structures/ComponentInteraction";
+export { default as DiscordHTTPError } from "./rest/DiscordHTTPError";
+export { default as DiscordRESTError } from "./rest/DiscordRESTError";
+export { default as ExtendedUser } from "./structures/ExtendedUser";
+export { default as ForumChannel } from "./structures/ForumChannel";
+export { default as GatewayError } from "./gateway/GatewayError";
+export { default as GroupChannel } from "./structures/GroupChannel";
+export { default as Guild } from "./structures/Guild";
+export { default as GuildChannel } from "./structures/GuildChannel";
+export { default as GuildPreview } from "./structures/GuildPreview";
+export { default as GuildScheduledEvent } from "./structures/GuildScheduledEvent";
+export { default as GuildTemplate } from "./structures/GuildTemplate";
+export { default as Integration } from "./structures/Integration";
+export { default as InteractionResolvedChannel } from "./structures/InteractionResolvedChannel";
+export { default as InteractionOptionsWrapper } from "./util/InteractionOptionsWrapper";
+export { default as Invite } from "./structures/Invite";
+export { default as Member } from "./structures/Member";
+export { default as Message } from "./structures/Message";
+export { default as ModalSubmitInteraction } from "./structures/ModalSubmitInteraction";
+export { default as PartialApplication } from "./structures/PartialApplication";
+export { default as Permission } from "./structures/Permission";
+export { default as PermissionOverwrite } from "./structures/PermissionOverwrite";
+export { default as PingInteraction } from "./structures/PingInteraction";
+export { default as PrivateChannel } from "./structures/PrivateChannel";
+export { default as PrivateThreadChannel } from "./structures/PrivateThreadChannel";
+export { default as PublicThreadChannel } from "./structures/PublicThreadChannel";
+export { default as RESTManager } from "./rest/RESTManager";
+export { default as Role } from "./structures/Role";
+export * as Routes from "./util/Routes";
+export { default as SequentialBucket } from "./rest/SequentialBucket";
+export { default as Shard } from "./gateway/Shard";
+export { default as ShardManager } from "./gateway/ShardManager";
+export { default as StageChannel } from "./structures/StageChannel";
+export { default as StageInstance } from "./structures/StageInstance";
+export { default as Team } from "./structures/Team";
+export { default as TextableChannel } from "./structures/TextableChannel";
+export { default as TextChannel } from "./structures/TextChannel";
+export { default as ThreadChannel } from "./structures/ThreadChannel";
+export { default as TypedCollection } from "./util/TypedCollection";
+export { default as TypedEmitter } from "./util/TypedEmitter";
+export { default as UnavailableGuild } from "./structures/UnavailableGuild";
+export { default as User } from "./structures/User";
+export { default as Util } from "./util/Util";
+export { default as VoiceChannel } from "./structures/VoiceChannel";
+export { default as VoiceState } from "./structures/VoiceState";
+export { default as Webhook } from "./structures/Webhook";
