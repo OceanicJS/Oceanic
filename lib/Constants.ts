377481c8812b650dd6e33020c02258a52dca4512
Donovan Daniels <hewwo@yiff.rocks>
2022-12-03 12:22:33 -0600

    more message types

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -509,20 +509,20 @@ export enum MessageFlags {
 }
 
 export enum MessageTypes {
-    DEFAULT                = 0,
-    RECIPIENT_ADD          = 1,
-    RECIPIENT_REMOVE       = 2,
-    CALL                   = 3,
-    CHANNEL_NAME_CHANGE    = 4,
-    CHANNEL_ICON_CHANGE    = 5,
-    CHANNEL_PINNED_MESSAGE = 6,
-    USER_JOIN              = 7,
-    GUILD_BOOST            = 8,
-    GUILD_BOOST_TIER_1     = 9,
-    GUILD_BOOST_TIER_2     = 10,
-    GUILD_BOOST_TIER_3     = 11,
-    CHANNEL_FOLLOW_ADD     = 12,
-
+    DEFAULT                                      = 0,
+    RECIPIENT_ADD                                = 1,
+    RECIPIENT_REMOVE                             = 2,
+    CALL                                         = 3,
+    CHANNEL_NAME_CHANGE                          = 4,
+    CHANNEL_ICON_CHANGE                          = 5,
+    CHANNEL_PINNED_MESSAGE                       = 6,
+    USER_JOIN                                    = 7,
+    GUILD_BOOST                                  = 8,
+    GUILD_BOOST_TIER_1                           = 9,
+    GUILD_BOOST_TIER_2                           = 10,
+    GUILD_BOOST_TIER_3                           = 11,
+    CHANNEL_FOLLOW_ADD                           = 12,
+    GUILD_STREAM                                 = 13,
     GUILD_DISCOVERY_DISQUALIFIED                 = 14,
     GUILD_DISCOVERY_REQUALIFIED                  = 15,
     GUILD_DISCOVERY_GRACE_PERIOD_INITIAL_WARNING = 16,
@@ -535,6 +535,13 @@ export enum MessageTypes {
     CONTEXT_MENU_COMMAND                         = 23,
     AUTO_MODERATION_ACTION                       = 24,
     ROLE_SUBSCRIPTION_PURCHASE                   = 25,
+    INTERACTION_PREMIUM_UPSELL                   = 26,
+    STAGE_START                                  = 27,
+    STAGE_END                                    = 28,
+    STAGE_SPEAKER                                = 29,
+    STAGE_RAISE_HAND                             = 30,
+    STAGE_TOPIC_CHANGE                           = 31,
+    GUILD_APPLICATION_PREMIUM_SUBSCRIPTION       = 32,
 }
 
 export enum MessageActivityTypes {
